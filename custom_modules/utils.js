cf55f0746c6fdd7f30e7ec8a9d9ea9e20b34d031
kounelios13 <pkounelios@gmail.com>
2017-07-12 00:35:57 +0300

    implemented rgb2hex function

--- custom_modules/utils.js
+++ custom_modules/utils.js
@@ -1,42 +1,46 @@
-let info = message =>{
+let info = message => {
     bootbox.alert({
-        title:'Info',
-        className:'dialog-info',
+        title: 'Info',
+        className: 'dialog-info',
         message
     });
 };
-let success = message =>{
+let success = message => {
     bootbox.alert({
-        title:'Success',
-        className:'dialog-success',
+        title: 'Success',
+        className: 'dialog-success',
         message
     });
 };
-let warning = message =>{
+let warning = message => {
     bootbox.alert({
-        title:'Warning',
-        className:'dialog-warning',
+        title: 'Warning',
+        className: 'dialog-warning',
         message
     });
 };
-let error = message =>{
+let error = message => {
     bootbox.alert({
-        title:'Error',
-        className:'dialog-error',
+        title: 'Error',
+        className: 'dialog-error',
         message
     })
 };
 //@TODO
-//rewrite arguments asdestructured object
-let confirm = (options)=>{
-    options = Object.assign({},{
-        title:'Confirm',
-        className:'dialog-warning'
-    },options);
+//rewrite arguments as destructured object
+let confirm = options => {
+    options = Object.assign({}, {
+        title: 'Confirm',
+        className: 'dialog-warning'
+    }, options);
     bootbox.confirm(options);
 };
-let rgb2hex = color =>{
-    return color;
+let rgb2hex = color => {
+    var rgbValues = color.split("(")[1].split(")")[0].split(",");
+    return `#` + rgbValues.map(col => {
+        col = parseInt(col).toString(16);
+        return (col.length == 1) ? `${0}col` : col;
+    }).join('');
 };
 module.exports = {
     info,
