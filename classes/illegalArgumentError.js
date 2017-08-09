93bbfc14968dda25d201a0d07bb1667214702868
kounelios13 <pkounelios@gmail.com>
2017-08-09 13:36:05 +0300

    fixed unstaged files

--- classes/illegalArgumentError.js
+++
@@ -1,7 +0,0 @@
-class IllegalArgumentError extends Error{
-    constructor(message){
-        super(message);
-        this.name = "IllegalArgumentError";
-    }
-}
-module.exports = IllegalArgumentError;
--- custom_modules/tray-menu.js
+++
@@ -1,8 +0,0 @@
-const {
-    ipcRenderer,
-    Tray,
-    Menu,
-} = require('electron');
-const path = require('path');
-const trayIcon = new Tray(`${__dirname}/icons/ugsm256x256.png`);
-module.exports = trayIcon;
